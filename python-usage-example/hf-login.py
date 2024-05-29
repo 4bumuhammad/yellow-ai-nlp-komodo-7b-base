714b5e4ff62bb4800cd4269aeb92f7978d0f62fa
Dhony Abu Muhammad <76083017+4bumuhammad@users.noreply.github.com>
2024-05-29 08:23:19 +0700

    updates

--- python-usage-example/hf-login.py
+++ python-usage-example/hf-login.py
@@ -1,4 +1,3 @@
 import huggingface_hub
 
 huggingface_hub.login("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") #TOKEN | token-grained-komodo-7b-base 
-# huggingface_hub.login("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") #TOKEN | test-write  
