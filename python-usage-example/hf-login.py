8cb64218af980ff248c6241caa8ea696a15633fe
Dhony Abu Muhammad <76083017+4bumuhammad@users.noreply.github.com>
2024-05-29 08:32:38 +0700

    updates

--- python-usage-example/hf-login.py
+++ python-usage-example/hf-login.py
@@ -1,3 +1,5 @@
 import huggingface_hub
 
 huggingface_hub.login("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") #TOKEN | token-grained-komodo-7b-base 
+
+# note: the token is only an example and cannot be reused as it has been removed from the current settings.
